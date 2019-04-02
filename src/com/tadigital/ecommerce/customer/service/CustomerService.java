5e1ed735d953698a7a801d58a576222ac7109f50
Sowmya Sree <sowmyal1998@gmail.com>
2019-04-02 18:42:57 +0530

    CustomerService is added to the repository

---
+++ src/com/tadigital/ecommerce/customer/service/CustomerService.java
@@ -0,0 +1,211 @@
+/*package com.tadigital.ecommerce.customer.service;
+
+//import com.sun.xml.internal.fastinfoset.sax.Properties;
+
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import com.tadigital.ecommerce.customer.dao.CustomerDao;
+import com.tadigital.ecommerce.customer.entity.Customer;
+
+
+public class CustomerService {
+	
+	CustomerDao cd = new CustomerDao();
+	
+	public boolean registerCustomer(Customer c) {
+		boolean status = cd.insertCustomer(c);
+		if(status) {
+			sendEmail(c.getFirstName()+" "+c.getLastName(),c.getEmail());
+		}
+		
+		return status;
+	}
+	public boolean loginCustomer(Customer c) {
+		boolean s=cd.selectCustomerByEmailAndPassword(c);
+		return s;
+	}
+	public String sendEmail(String name,String mail)
+	{
+		String status="NOT SENT";
+		Properties properties=new Properties();
+		properties.put("mail.smtp.host", "smtp.gmail.com");
+		properties.put("mail.smtp.socketFactory.port", "465");
+		properties.put("mail.smtp.socketFactory.class",	"javax.net.ssl.SSLSocketFactory");
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.port", "465");
+		
+		//CONNECT TO MAIL SERVER
+		Session session = Session.getDefaultInstance(properties,new javax.mail.Authenticator() {
+																	protected PasswordAuthentication getPasswordAuthentication() {
+																		return new PasswordAuthentication("sowmyal1998@gmail.com","xxxx");
+																	}
+																});
+		try {
+			//COMPOSE MESSAGE
+			MimeMessage mimeMessage = new MimeMessage(session);
+			mimeMessage.setFrom(new InternetAddress("sowmyal1998@gmail.com"));
+			mimeMessage.setRecipients(Message.RecipientType.TO,	InternetAddress.parse(mail));
+			mimeMessage.setSubject("Welcome to the world of TA Digital!");
+			MimeMultipart mp = new MimeMultipart();
+			MimeBodyPart mbp1= new MimeBodyPart();
+			
+			String msg ="<body bgcolor='AliceBlue'>Dear <i><font color='DarkBlue'>" + name + "</font></i>,<br/><br/>" +
+			"<img src='cid:img1'><br/><h2><font color='#C70039'>Welcome to TA Digital Family!</font></h2>" +
+			"<br/><font color='brown'>We are excited to have you on-board.</font>" +
+			"<br/><br/><br/><font color='violet'>Stay tuned and we have got some great things in store for you.</font>" +
+			"<br/><br/><br/><font color='#FFC300'>Happy Shopping!<br/>TA Digital<br/><br/></font><img src='cid:img2'></body>";
+			
+			mbp1.setContent(msg,"text/html");
+			mp.addBodyPart(mbp1);
+
+
+			mbp1 = new MimeBodyPart();
+			DataSource fds = new FileDataSource("E:/Trainee Engineers March 2019/workspace/TrainingProject_Sowmya/WebContent/images/logo.png");
+			mbp1.setDataHandler(new DataHandler(fds));
+			mbp1.setHeader("Content-ID", "<img1>");
+			mp.addBodyPart(mbp1);
+
+			mbp1 = new MimeBodyPart();
+			fds = new FileDataSource("E:/Trainee Engineers March 2019/workspace/TrainingProject_Sowmya/WebContent/images/welcome.jpg");
+			mbp1.setDataHandler(new DataHandler(fds));
+			mbp1.setHeader("Content-ID", "<img2>");
+			mp.addBodyPart(mbp1);
+
+			mimeMessage.setContent(mp);mimeMessage.setText(msg);
+
+			//SEND MAIL
+			Transport.send(mimeMessage);
+			
+			status = "SENT";
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+		}
+		
+		return status;
+	}
+	
+
+}*/
+
+package com.tadigital.ecommerce.customer.service;
+
+import java.util.ArrayList;
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import com.tadigital.ecommerce.customer.dao.CustomerDao;
+import com.tadigital.ecommerce.customer.entity.Customer;
+
+public class CustomerService {
+CustomerDao CustomerDao = new CustomerDao();
+
+public boolean loginCustomer(Customer customer) {
+boolean status = CustomerDao.selectCustomerByEmailAndPassword(customer);
+
+return status;
+}
+
+public boolean registerCustomer(Customer customer) {
+boolean status = CustomerDao.insertCustomer(customer);
+
+if(status) {
+sendWelcomeMail(customer.getFirstName() + " " + customer.getLastName(), customer.getEmail());
+}
+
+return status;
+}
+
+
+
+
+
+public String sendWelcomeMail(String name, String email) {
+String status = "NOT SENT";
+
+//MAIL SERVER CONFIGURATION
+Properties properties = new Properties();
+properties.put("mail.smtp.host", "smtp.gmail.com");
+properties.put("mail.smtp.socketFactory.port", "465");
+properties.put("mail.smtp.socketFactory.class",	"javax.net.ssl.SSLSocketFactory");
+properties.put("mail.smtp.auth", "true");
+properties.put("mail.smtp.port", "465");
+
+//CONNECT TO MAIL SERVER
+Session session = Session.getDefaultInstance(properties,new javax.mail.Authenticator() {
+protected PasswordAuthentication getPasswordAuthentication() {
+return new PasswordAuthentication("sowmyal1998@gmail.com","xxxx");
+}
+});
+try {
+//COMPOSE MESSAGE
+MimeMessage mimeMessage = new MimeMessage(session);
+mimeMessage.setFrom(new InternetAddress("sowmyal1998@gmail.com"));
+mimeMessage.setRecipients(Message.RecipientType.TO,	InternetAddress.parse(email));
+mimeMessage.setSubject("Welcome to the world of TA Digital!");
+MimeMultipart mp = new MimeMultipart();
+MimeBodyPart mbp1= new MimeBodyPart();
+
+String msg ="<body bgcolor='#DAF7A6'>Dear <i><font color='#FF7A33'>" + name + "</font></i>,<br/><br/>" +
+"<img src='cid:image1'><br/><h2><font color='#C70039'>Welcome to TA Digital Family!</font></h2>" +
+"<br/><font color='#900C3F'>We are excited to have you on-board.</font>" +
+"<br/><br/><br/><font color='#581845'>Stay tuned and we have got some great things in store for you.</font>" +
+"<br/><br/><br/><font color='#FFC300'>Happy Shopping!<br/>TA Digital<br/><br/></font><img src='cid:image'></body>";
+mbp1.setContent(msg,"text/html");
+mp.addBodyPart(mbp1);
+
+
+mbp1 = new MimeBodyPart();
+DataSource fds = new FileDataSource("E://Trainee Engineers March 2019/workspace/TrainingProject_Sowmya/WebContent/images/logo.png");
+mbp1.setDataHandler(new DataHandler(fds));
+mbp1.setHeader("Content-ID", "<image>");
+mp.addBodyPart(mbp1);
+
+mbp1 = new MimeBodyPart();
+fds = new FileDataSource("E://Trainee Engineers March 2019/workspace/TrainingProject_Sowmya/WebContent/images/welcome.jpg");
+mbp1.setDataHandler(new DataHandler(fds));
+mbp1.setHeader("Content-ID", "<image1>");
+mp.addBodyPart(mbp1);
+
+mimeMessage.setContent(mp);
+
+//SEND MAIL
+Transport.send(mimeMessage);
+
+status = "SENT";
+} catch (MessagingException mex) {
+mex.printStackTrace();
+}
+
+return status;
+}
+}
+
+
+
+
+
+
