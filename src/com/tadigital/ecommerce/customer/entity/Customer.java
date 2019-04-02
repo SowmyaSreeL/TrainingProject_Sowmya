4486c779716f2ea56faa6e2c37c537b517a6dded
Sowmya Sree <sowmyal1998@gmail.com>
2019-04-02 18:42:22 +0530

    Customer entity is added to the repository

---
+++ src/com/tadigital/ecommerce/customer/entity/Customer.java
@@ -0,0 +1,53 @@
+package com.tadigital.ecommerce.customer.entity;
+
+import java.util.Calendar;
+
+public class Customer {
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	
+	public Customer() {
+	}
+	public Customer(String firstName, String lastName, String email, String password) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return "Customer [firstName=" + firstName + ", lastName=" + 
+				lastName + ", email=" + email + 
+				", password=" + password + "]";
+	}
+	
+}
